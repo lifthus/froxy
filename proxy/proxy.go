7b6c213e66cef953765adc3a2f8e3c717e49d462
lifthus <lifthus531@gmail.com>
2023-08-10 23:30:42 +0900

    proxy.Start

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,10 +1,13 @@
 package proxy
 
+import "goroxy/config"
+
 /*
 This package implements the proxy server.
 */
 
 // Start starts the proxy server with the configurations.
-func Start() {
+func Start(conf config.Config) error {
 	// TODO
+	return nil
 }
