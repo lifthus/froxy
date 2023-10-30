fbc0e6d564799019bee69571d029e2b5e769f729
lifthus <lifthus531@gmail.com>
2023-10-31 00:18:40 +0900

    config done except self-signing

--- init/config/config.go
+++ init/config/config.go
@@ -1,9 +1,11 @@
 package config
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"net/url"
+	"os"
 	"regexp"
 	"strings"
 
@@ -30,25 +32,21 @@ type Dashboard struct {
 	// TLSConfig holds the HTTPS configurations for the dashboard.
 	// HTTPS is mandatory for using the web dashboard.
 	// If you don't provide key pair, Froxy will generate self-signed key pair for itself.
-	TLSConfig tls.Config
-}
-type Secure struct {
-	Cert string
-	Key  string
+	TLSConfig *tls.Config
 }
 
 func InitConfig() (*FroxyConfig, error) {
 	fRootID := flag.String("id", "", "dashboard root id")
 	fRootPW := flag.String("pw", "", "dashboard root password")
-	fCert := flag.String("cert", "", "dashboard https cert file, self-signed if not provided")
-	fKey := flag.String("key", "", "dashboard https key file, self-signed if not provided")
+	fCertPath := flag.String("cert", "", "dashboard https cert file, self-signed if not provided")
+	fKeyPath := flag.String("key", "", "dashboard https key file, self-signed if not provided")
 	fPort := flag.String("port", "8542", "dashboard port number")
 	flag.Parse()
 
 	fconfig := &FroxyConfig{}
 	var err error
 
-	fconfig.DashBoard, err = initDashboard(*fRootID, *fRootPW, *fPort, *fCert, *fKey)
+	fconfig.DashBoard, err = initDashboard(*fRootID, *fRootPW, *fPort, *fCertPath, *fKeyPath)
 	if err != nil {
 		return nil, err
 	}
@@ -61,12 +59,22 @@ func InitConfig() (*FroxyConfig, error) {
 	return fconfig, nil
 }
 
-func initDashboard(rootID, rootPW, port, cert, key string) (*Dashboard, error) {
+func initDashboard(rootID, rootPW, port, certPath, keyPath string) (*Dashboard, error) {
 	dsbd := &Dashboard{}
 	if isDashboardDisabled(rootID, rootPW) {
 		return nil, nil
 	}
 	err := validateRootCredentials(rootID, rootPW)
+	if err != nil {
+		return nil, err
+	}
+	dsbd.RootID = rootID
+	dsbd.RootPW = rootPW
+	dsbd.Port, err = validateAndFormatPort(port)
+	if err != nil {
+		return nil, err
+	}
+	dsbd.TLSConfig, err = initTLSConfig(certPath, keyPath)
 	if err != nil {
 		return nil, err
 	}
@@ -82,20 +90,73 @@ func validateRootCredentials(rootID, rootPW string) error {
 	if err != nil {
 		return err
 	} else if !idMatched {
-		return fmt.Errorf("root id must be 5~20 characters(only numbers, english alphabets and underscore) long starting with an alphabet")
+		return fmt.Errorf("root id must be 5~20 characters(only digits, english alphabets and underscore) long starting with an alphabet")
 	}
 	pwMatched, err := regexp.MatchString("^[a-zA-Z0-9_!@#$%^&*]*[_!@#$%^&*]+[a-zA-Z0-9_!@#$%^&*]*$", rootPW)
 	if err != nil {
 		return err
 	} else if !pwMatched || len(rootPW) < 6 || len(rootPW) > 100 {
-		return fmt.Errorf("root password must be 6~100 characters(only numbers, english alphabets and at least one between _!@#$%%^&*) long")
+		return fmt.Errorf("root password must be 6~100 characters(only digits, english alphabets and at least one between _!@#$%%^&*) long")
 	}
 	return nil
 }
 
+func validateAndFormatPort(port string) (string, error) {
+	portMatched, err := regexp.MatchString("^:?\\d{1,5}$", port)
+	if err != nil {
+		return "", err
+	} else if !portMatched {
+		return "", fmt.Errorf("port number must be 1~5 digits long")
+	}
+	return ":" + strings.TrimPrefix(port, ":"), nil
+}
+
+func initTLSConfig(certPath, keyPath string) (*tls.Config, error) {
+	if certPath == "" && keyPath == "" {
+		return signTLSCertSelf()
+	}
+	return loadTLSConfig(certPath, keyPath)
+}
+
+func signTLSCertSelf() (*tls.Config, error) {
+	// TODO: check outbind IP addr and generate self-signed cert with it(including localhost and 127.0.0.1).
+	return nil, fmt.Errorf("self-signed cert generation is not implemented yet")
+}
+
+func loadTLSConfig(certPath, keyPath string) (*tls.Config, error) {
+	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
+	if err != nil {
+		return nil, err
+	}
+	return &tls.Config{Certificates: []tls.Certificate{cert}}, nil
+}
+
 func initFroxyfile() (*froxyfile.FroxyfileConfig, error) {
-	ffc := &froxyfile.FroxyfileConfig{}
-	return ffc, nil
+	ffb, err := tryOpeningAndReadFroxyfile([]string{"froxyfile", "froxyfile.yml", "froxyfile.yaml"})
+	if err != nil {
+		return nil, err
+	}
+	return froxyfile.Parse(ffb)
+}
+
+func tryOpeningAndReadFroxyfile(paths []string) ([]byte, error) {
+	var err error
+	var ff *os.File
+	for i, path := range paths {
+		ff, err = os.Open(path)
+		if errors.Is(err, os.ErrNotExist) && i < len(paths)-1 {
+			continue
+		} else if err != nil {
+			return nil, err
+		}
+		break
+	}
+	ffb := make([]byte, 1000000)
+	if n, err := ff.Read(ffb); err != nil {
+		return nil, err
+	} else {
+		return ffb[:n], nil
+	}
 }
 
 type Args struct {
@@ -103,6 +164,11 @@ type Args struct {
 	Port            string
 	Target          *url.URL
 	LoadBalanceList []*url.URL
+}
+
+type Secure struct {
+	Cert string
+	Key  string
 }
 
 func InitArgsAndTargets() (args *Args, err error) {
