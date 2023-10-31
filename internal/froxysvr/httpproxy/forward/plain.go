9e1f43a125ce7e7e7b8c4b0ba0b829f898a94337
lifthus <lifthus531@gmail.com>
2023-11-01 00:30:36 +0900

    plain forward proxy

--- internal/froxysvr/httpproxy/forward/plain.go
+++ internal/froxysvr/httpproxy/forward/plain.go
@@ -1,16 +1,22 @@
 package forward
 
 import (
+	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
-type PlainForwardProxy struct{}
+// PlainForwardProxy is a forward proxy, which the feature is developed in plain way.
+type PlainForwardProxy struct {
+	// ForwardChainInfo is a flag to indicate whether to forward the proxy chain info.
+	ForwardChainInfo bool
+}
 
 func (pfp PlainForwardProxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// TODO: log management with on/off switch
 	// log.Println(req.RemoteAddr, "\t", req.Method, "\t", req.URL, "\t Host:", req.Host)
 	// log.Println("\t\t", req.Header)
 
@@ -20,40 +26,56 @@ func (pfp PlainForwardProxy) ServeHTTP(w http.ResponseWriter, req *http.Request)
 		return
 	}
 
-	if req.URL.Scheme != "http" && req.URL.Scheme != "https" {
-		msg := "unsupported scheme " + req.URL.Scheme
-		http.Error(w, msg, http.StatusBadRequest)
-		// log.Println(msg)
+	if !IsSchemeHTTPOrHTTPS(req.URL) {
+		http.Error(w, "unsupported scheme "+req.URL.Scheme, http.StatusBadRequest)
 		return
 	}
 
-	client := &http.Client{}
-	req.RequestURI = "" // error if it is set.
-
+	removeHeadersInConnectionHeader(req.Header)
 	removeHopHeaders(req.Header)
-	removeConnectionHeaders(req.Header)
 
-	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
-		appendHostToXForwardHeader(req.Header, clientIP)
+	if pfp.ForwardChainInfo {
+		appendSenderAddrToXForwaredForHeader(req.Header, req.RemoteAddr)
+		appendSenderAddrToForwardedHeader(req.Header, req)
 	}
 
-	resp, err := client.Do(req)
+	client := &http.Client{}
+	resp, err := client.Do(reqWithClearedRequestURI(req))
 	if err != nil {
-		http.Error(w, "Server Error", http.StatusInternalServerError)
-		log.Fatal("ServeHTTP:", err)
+		http.Error(w, "server-side request error", http.StatusInternalServerError)
+		return
 	}
 	defer resp.Body.Close()
 
-	log.Println(req.RemoteAddr, " ", resp.Status)
+	//log.Println(req.RemoteAddr, " ", resp.Status)
 
+	removeHeadersInConnectionHeader(resp.Header)
 	removeHopHeaders(resp.Header)
-	removeConnectionHeaders(resp.Header)
 
 	copyHeader(w.Header(), resp.Header)
 	w.WriteHeader(resp.StatusCode)
+
 	io.Copy(w, resp.Body)
 }
 
+func IsSchemeHTTPOrHTTPS(url *url.URL) bool {
+	return url.Scheme == "http" || url.Scheme == "https"
+}
+
+// removeHeadersInConnection removes headers in Connection field.
+// The Connection header field allows the sender to specify options that are desired "only for transport-level that connection".
+func removeHeadersInConnectionHeader(hd http.Header) http.Header {
+	for _, f := range hd["Connection"] {
+		for _, sf := range strings.Split(f, ",") {
+			if sf = strings.TrimSpace(sf); sf != "" {
+				hd.Del(sf)
+			}
+		}
+	}
+	return hd
+}
+
+// RFC 7230 does not have such an explicit hop-by-hop header list and is not concerned.
 var hopHeaders = []string{
 	"Connection",
 	"Proxy-Connection",
@@ -66,30 +88,42 @@ var hopHeaders = []string{
 	"Upgrade",
 }
 
-func removeHopHeaders(header http.Header) {
+func removeHopHeaders(hd http.Header) {
 	for _, h := range hopHeaders {
-		header.Del(h)
+		hd.Del(h)
 	}
 }
 
-func removeConnectionHeaders(h http.Header) {
-	for _, f := range h["Connection"] {
-		for _, sf := range strings.Split(f, ",") {
-			if sf = strings.TrimSpace(sf); sf != "" {
-				h.Del(sf)
-			}
-		}
+func appendSenderAddrToXForwaredForHeader(hd http.Header, remoteAddr string) {
+	sender, _, err := net.SplitHostPort(remoteAddr)
+	if err != nil {
+		return
 	}
+	if prev, ok := hd["X-Forwarded-For"]; ok {
+		sender = strings.Join(prev, ", ") + ", " + sender
+	}
+	hd.Set("X-Forwarded-For", sender)
 }
 
-func appendHostToXForwardHeader(header http.Header, host string) {
-	// If we aren't the first proxy retain prior
-	// X-Forwarded-For information as a comma+space
-	// separated list and fold multiple headers into one.
-	if prior, ok := header["X-Forwarded-For"]; ok {
-		host = strings.Join(prior, ", ") + ", " + host
+func appendSenderAddrToForwardedHeader(hd http.Header, req *http.Request) {
+	sender, _, err := net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		return
 	}
-	header.Set("X-Forwarded-For", host)
+	sender = fmt.Sprintf("by=%s;for=%s;host=%s;proto=%s", "hidden", sender, req.Host, req.URL.Scheme)
+	if prev, ok := hd["Forwarded"]; ok {
+		sender = strings.Join(prev, ", ") + sender
+	}
+	hd.Set("Forwarded", sender)
+}
+
+// ReqWithClearedRequestURI clears req.RequestURI.
+// RequestURI field causes error if it is set in the HTTP Client request.
+// It is set when the server receives an request, by parsing the request line(e.g. GET http://www.example.com/ HTTP/1.1)'s request target.
+// Clear it to reuse the request.
+func reqWithClearedRequestURI(req *http.Request) *http.Request {
+	req.RequestURI = ""
+	return req
 }
 
 func copyHeader(dst, src http.Header) {
