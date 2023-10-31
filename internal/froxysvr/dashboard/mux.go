392eaaa3f934c51a4b20550678000b015b3b9c0f
lifthus <lifthus531@gmail.com>
2023-10-31 20:55:31 +0900

    dashboard

--- internal/froxysvr/dashboard/mux.go
+++ internal/froxysvr/dashboard/mux.go
@@ -3,5 +3,8 @@ package dashboard
 import "net/http"
 
 func MuxDashboardAPI(mux *http.ServeMux) *http.ServeMux {
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Dashboard still not supported"))
+	})
 	return mux
 }
