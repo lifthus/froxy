569996017173b689a344fbf3fed01009f75e17fa
lifthus <lifthus531@gmail.com>
2023-11-01 00:40:16 +0900

    misc

--- internal/froxysvr/httpproxy/forward/plain.go
+++ internal/froxysvr/httpproxy/forward/plain.go
@@ -75,7 +75,11 @@ func removeHeadersInConnectionHeader(hd http.Header) http.Header {
 	return hd
 }
 
-// RFC 7230 does not have such an explicit hop-by-hop header list and is not concerned.
+// Hop-by-hop headers. These are removed when sent to the backend.
+// As of RFC 7230, hop-by-hop headers are required to appear in the
+// Connection header field. These are the headers defined by the
+// obsoleted RFC 2616 (section 13.5.1) and are used for backward
+// compatibility.
 var hopHeaders = []string{
 	"Connection",
 	"Proxy-Connection",
